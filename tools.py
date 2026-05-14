abf6cea4acbcb3db2df6499e1169f5dfdce5ab33
fahiq7 <fahiquemuhammed099@gmail.com>
2026-05-14 16:32:50 +0530

    fix coin price key error

--- tools.py
+++ tools.py
@@ -1,10 +1,20 @@
 import requests
 import feedparser
 
-def get_price(coin_id):
-    url = f"https://api.coingecko.com/api/v3/simple/price?ids={coin_id}&vs_currencies=usd"
-    data = requests.get(url).json()
-    return data[coin_id]["usd"]
+def get_price(coin_id="bitcoin"):
+    url = "https://api.coingecko.com/api/v3/simple/price"
+    params = {
+        "ids": coin_id,
+        "vs_currencies": "usd"
+    }
+
+    response = requests.get(url)
+    data = response.json()
+
+    if coin_id in data:
+        return data[coin_id]["usd"]
+    else:
+        return "Coin not found"
 
 
 def get_news():
